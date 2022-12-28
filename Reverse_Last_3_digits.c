cfb7a8aba83a55701847c3321033a0fc6c67cc89
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2022-12-28 23:01:29 +0530

    #include<stdio.h> int main() {     int a,r,s=0,q,t;     scanf("%d",&a);     r=a/1000;     q=a%1000;     while(q!=0)     {         t=q%10;         s=s*10+t;         q=q/10;     }     printf("%d%d",r,s); }

---
+++ Reverse_Last_3_digits.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,r,s=0,q,t;
+    scanf("%d",&a);
+    r=a/1000;
+    q=a%1000;
+    while(q!=0)
+    {
+        t=q%10;
+        s=s*10+t;
+        q=q/10;
+    }
+    printf("%d%d",r,s);
+}
