1fb2723938591043bf275cb73c2b9567096a8f35
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2022-12-29 17:10:03 +0530

    #include<stdio.h> int main() {   int a,i;   scanf("%d",&a);   for(i=0;i<a;i++)   {       int x;       scanf("%d",&x);    if(x>=50)    printf("YES ");    else    printf("NO ");   } }

---
+++ Subscription.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int a,i;
+  scanf("%d",&a);
+  for(i=0;i<a;i++)
+  {
+      int x;
+      scanf("%d",&x);
+   if(x>=50)
+   printf("YES
+");
+   else 
+   printf("NO
+");
+  }
+}
