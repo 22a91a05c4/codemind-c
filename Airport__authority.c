e5a6e9fc6d9e404590331db252bb2091326e37ff
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-03-31 21:35:40 +0530

    #include<stdio.h> int main() {     int a[100],i,n,c=0,b=0,ans,k;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     int m;     scanf("%d",&m);     for(i=0;i<n;i++)     {         if(a[i]<=m)         {             c++;         }         else         {             b++;         }     }     k=b*2;     ans=c+k;     printf("%d",ans);
    }

---
+++ Airport__authority.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],i,n,c=0,b=0,ans,k;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int m;
+    scanf("%d",&m);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<=m)
+        {
+            c++;
+        }
+        else
+        {
+            b++;
+        }
+    }
+    k=b*2;
+    ans=c+k;
+    printf("%d",ans);
+    
+}
