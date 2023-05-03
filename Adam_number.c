31397615bd72f3725467a19005122ca575a1bad4
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-05-03 19:04:41 +0530

    #include<stdio.h> int main() {     int a,r,s=0,p,q,m,n=0;     scanf("%d",&a);     p=a*a;     while(a)     {         r=a%10;         s=s*10+r;         a=a/10;     }     q=s*s;     while(q)     {         m=q%10;         n=n*10+m;         q=q/10;     }     if(n==p)     {         printf("True");     }     else     {         printf("False");     } }

--- Adam_number.c
+++ Adam_number.c
@@ -1,24 +1,28 @@
 #include<stdio.h>
 int main()
 {
-    int a,r,p,sq1,sq2,s1=0,s2=0;
+    int a,r,s=0,p,q,m,n=0;
     scanf("%d",&a);
-    sq1=a*a;
-    while(a!=0)
+    p=a*a;
+    while(a)
     {
         r=a%10;
-        s1=s1*10+r;
+        s=s*10+r;
         a=a/10;
     }
-    sq2=s1*s1;
-    while(sq2!=0)
+    q=s*s;
+    while(q)
     {
-        p=sq2%10;
-        s2=s2*10+p;
-        sq2=sq2/10;
+        m=q%10;
+        n=n*10+m;
+        q=q/10;
     }
-    if(s2==sq1)
-    printf("True");
+    if(n==p)
+    {
+        printf("True");
+    }
     else
-    printf("False");
+    {
+        printf("False");
+    }
 }
