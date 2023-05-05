2b3ba8054e81ea69b533428154cf3c69629e4977
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-05-05 21:28:29 +0530

    #include<stdio.h> int main() {     int n,a=0,b=1,c,i;     scanf("%d",&n);     for(i=0;i<n;i++)     {         printf("%d ",a);         c=a+b;         a=b;         b=c;     } }

--- fibonacci.c
+++ fibonacci.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int a=0,b=1,c,i,n;
+    int n,a=0,b=1,c,i;
     scanf("%d",&n);
-    for(i=1;i<=n;i++)
+    for(i=0;i<n;i++)
     {
-         printf("%d ",a);
-       c=a+b;
-       a=b;
-       b=c;
+        printf("%d ",a);
+        c=a+b;
+        a=b;
+        b=c;
     }
-    
 }
