94b1baa388d916df8054db791ef517ec9bfa2805
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-08-10 18:25:06 +0530

    #include<stdio.h> #include<math.h> int main() {     int i;     for(i=1;i>=1;i++)     {         int a,b;         scanf("%d",&a);         if(a!=-1)         {             b=(a*a);             printf("%d ",b);         }         else         {             break;         }     } }

---
+++ Infinite.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i;
+    for(i=1;i>=1;i++)
+    {
+        int a,b;
+        scanf("%d",&a);
+        if(a!=-1)
+        {
+            b=(a*a);
+            printf("%d
+",b);
+        }
+        else
+        {
+            break;
+        }
+    }
+}
