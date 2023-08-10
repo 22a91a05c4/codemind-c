a089ab98c70051cea11ceaa2c5ad86e93d1b123a
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-08-10 17:52:29 +0530

    #include<stdio.h> int main() {     int a[100],n,p,q,i,s=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     scanf("%d%d",&p,&q);     for(i=0;i<n;i++)     {         if(a[i]<p || a[i]>q)         {             s=s+a[i];         }     }     printf("%d",s); }

---
+++ Sum_of_elements_not_between_A_and_B.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int a[100],n,p,q,i,s=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    scanf("%d%d",&p,&q);
+    for(i=0;i<n;i++)
+    {
+        if(a[i]<p || a[i]>q)
+        {
+            s=s+a[i];
+        }
+    }
+    printf("%d",s);
+}
