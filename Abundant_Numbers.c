f02a2b6983c751e266e44b15757e0b73d6faaef5
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-08-13 14:27:42 +0530

    #include<stdio.h> int main() {     int a,i,s=0,t;     scanf("%d",&a);     t=a;     for(i=1;i<a;i++)     {         if(a%i==0)         {             s=s+i;         }     }     if(s>=t)     {         printf("True");     }     else     {         printf("False");     } }

--- Abundant_Numbers.c
+++ Abundant_Numbers.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 int main()
 {
-    int a,i,s=0;
+    int a,i,s=0,t;
     scanf("%d",&a);
+    t=a;
     for(i=1;i<a;i++)
     {
         if(a%i==0)
@@ -10,7 +11,7 @@ int main()
             s=s+i;
         }
     }
-    if(s>a)
+    if(s>=t)
     {
         printf("True");
     }
