6e035b9a648ffe72bbf45fa1c0ff64422b8f0748
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-08-26 14:12:07 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     if(a%3==0)     {         printf("Pling");     }     if(a%5==0)     {         printf("Plang");     }     if(a%7==0)     {         printf("Plong");     }     else if(a%3!=0 && a%5!=0 && a%7!=0)     {         printf("%d",a);     } }

--- RainDrop.c
+++ RainDrop.c
@@ -3,28 +3,20 @@ int main()
 {
     int a;
     scanf("%d",&a);
-    if(a%3==0 && a%7!=0 && a%5!=0)
+    if(a%3==0)
     {
         printf("Pling");
     }
-    else if(a%3!=0 && a%7==0 && a%5!=0)
-    {
-        printf("Plong");
-    }
-    else if(a%3!=0 && a%7!=0 && a%5==0)
+    if(a%5==0)
     {
         printf("Plang");
     }
-    else if(a%3==0 && a%5==0 && a%7!=0)
+    if(a%7==0)
     {
-        printf("PlingPlang");
+        printf("Plong");
     }
-    else if(a%3==0 && a%5!=0 && a%7==0)
+    else if(a%3!=0 && a%5!=0 && a%7!=0)
     {
-        printf("PlingPlong");
-    }
-    else if(a%3!=0 && a%5==0 && a%7==0)
-    {
-        printf("PlangPlong");
+        printf("%d",a);
     }
 }
