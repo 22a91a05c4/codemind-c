d6c4a9f3e52be0b3c1870b35d5cd25c773ddd7e7
Ganugula Satya Sathvika <119840088+22a91a05c4@users.noreply.github.com>
2023-10-02 14:23:59 +0530

    #include<stdio.h> int main() {     int r,c,i,j,a[100][100],s=0,o=0;     scanf("%d%d",&r,&c);     for(i=0;i<r;i++)     {         for(j=0;j<c;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {         if(i%2==0)         {             for(j=0;j<c;j++)             {                 s=s+a[i][j];             }         }         else         {             for(j=0;j<c;j++)             {                 o=o+a[i][j];             }         }     }     printf("%d %d",s,o); }

---
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int r,c,i,j,a[100][100],s=0,o=0;
+    scanf("%d%d",&r,&c);
+    for(i=0;i<r;i++)
+    {
+        for(j=0;j<c;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+    for(i=0;i<r;i++)
+    {
+        if(i%2==0)
+        {
+            for(j=0;j<c;j++)
+            {
+                s=s+a[i][j];
+            }
+        }
+        else
+        {
+            for(j=0;j<c;j++)
+            {
+                o=o+a[i][j];
+            }
+        }
+    }
+    printf("%d %d",s,o);
+}
